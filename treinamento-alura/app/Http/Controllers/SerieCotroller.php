4209df2407d19376472e4c4ee1378b7bed6e5602
VictorNTW <victor1992hm@gmail.com>
2024-05-10 22:31:54 -0300

    init laravel

---
+++ treinamento-alura/app/Http/Controllers/SerieCotroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SerieCotroller extends Controller
+{
+    public function listarSeries(){
+        echo "teste";
+    }
+}
